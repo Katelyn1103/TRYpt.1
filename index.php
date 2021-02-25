10f9f9957deb731f79cdefdfb2c786a58ae00c52
Katelyn1103 <77051874+Katelyn1103@users.noreply.github.com>
2021-02-25 22:44:15 +0800

    Update index.php

--- index.php
+++ index.php
@@ -1,5 +1,4 @@
 <html>
-<head><title></title></head>
 <frameset rows="8%,13%,79%", frameborder="no">
  <frame frameborder="0", scrolling="no", Noresize="noresize", src="file:///C:/Users/ACER/Documents/TLE PT/FRAME1.html">
  <frame frameborder="0", scrolling="no", Noresize="noresize", src="file:///C:/Users/ACER/Documents/TLE PT/FRAME2.html">
